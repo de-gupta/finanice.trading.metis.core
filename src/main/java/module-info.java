87e516e9651bd846b09385eec6a9fbb2e4f711e5
shylock <shylock@apollo>
2026-06-11 17:56:12 +0200

    jpms

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module de.gupta.metis.core
+{
+	exports de.gupta.metis.core.types.number;
+	exports de.gupta.metis.core.types.currency;
+
+	exports de.gupta.metis.core.types.quoting;
+	exports de.gupta.metis.core.types.price;
+	exports de.gupta.metis.core.types.size;
+
+	exports de.gupta.metis.core.types.money;
+}

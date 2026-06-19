4422738bd26ab9f31fce3ca4c2724b0d59f05aa2
shylock <shylock@apollo>
2026-06-19 20:45:35 +0200

    factories

--- src/main/java/de/gupta/metis/core/types/price/quoted/QuotedPriceFactory.java
+++ src/main/java/de/gupta/metis/core/types/price/quoted/QuotedPriceFactory.java
@@ -7,15 +7,26 @@ import de.gupta.metis.core.types.quoting.PriceQuotingUnit;
 
 public final class QuotedPriceFactory
 {
+	public static <U extends PriceQuotingUnit> QuotedPrice<U> zero(final PriceQuotingConvention<U> convention)
+	{
+		return of(0, convention);
+	}
+
+	public static <U extends PriceQuotingUnit> QuotedPrice<U> of(final int price,
+	                                                             final PriceQuotingConvention<U> convention)
+	{
+		return of((long) price, convention);
+	}
+
+	public static <U extends PriceQuotingUnit> QuotedPrice<U> of(final long price,
+	                                                             final PriceQuotingConvention<U> convention)
+	{
+		return of(PriceTypeFactory.of(price), convention);
+	}
+
 	public static <U extends PriceQuotingUnit> QuotedPrice<U> of(final PriceType price,
 	                                                             final PriceQuotingConvention<U> convention)
 	{
 		return QuotedPriceImpl.of(price, convention);
 	}
-
-	public static <U extends PriceQuotingUnit> QuotedPrice<U> zero(final PriceQuotingConvention<U> convention)
-	{
-		return QuotedPriceImpl.of(PriceTypeFactory.zero(), convention);
-	}
-
 }
--- src/main/java/de/gupta/metis/core/types/size/quoted/QuotedSizeFactory.java
+++ src/main/java/de/gupta/metis/core/types/size/quoted/QuotedSizeFactory.java
@@ -3,9 +3,27 @@ package de.gupta.metis.core.types.size.quoted;
 import de.gupta.metis.core.types.quoting.SizeQuotingConvention;
 import de.gupta.metis.core.types.quoting.SizeQuotingUnit;
 import de.gupta.metis.core.types.size.SizeType;
+import de.gupta.metis.core.types.size.SizeTypeFactory;
 
 public final class QuotedSizeFactory
 {
+	public static <U extends SizeQuotingUnit> QuotedSize<U> zero(final SizeQuotingConvention<U> convention)
+	{
+		return of(0, convention);
+	}
+
+	public static <U extends SizeQuotingUnit> QuotedSize<U> of(final int size,
+	                                                           final SizeQuotingConvention<U> convention)
+	{
+		return of((long) size, convention);
+	}
+
+	public static <U extends SizeQuotingUnit> QuotedSize<U> of(final long size,
+	                                                           final SizeQuotingConvention<U> convention)
+	{
+		return of(SizeTypeFactory.of(size), convention);
+	}
+
 	public static <U extends SizeQuotingUnit> QuotedSize<U> of(final SizeType size,
 	                                                           final SizeQuotingConvention<U> convention)
 	{
